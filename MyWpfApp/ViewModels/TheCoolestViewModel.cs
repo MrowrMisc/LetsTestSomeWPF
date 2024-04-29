68bdc2bbbc51c93b8b151bd9d41b212f10603fa6
Mrowr Purr <mrowr.purr@gmail.com>
2024-04-29 07:27:31 -0700

    Add some fields with bindings for testing!

---
+++ MyWpfApp/ViewModels/TheCoolestViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace MyWpfApp;
+
+public class TheCoolestViewModel : INotifyPropertyChanged
+{
+    private string? _myTextProperty;
+
+    public string? MyTextProperty
+    {
+        get { return _myTextProperty; }
+        set
+        {
+            _myTextProperty = value;
+            OnPropertyChanged();
+        }
+    }
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
